9b346f3297a96e8d031164b25ae464c3b728d184
jhenn reis <101225404+jhereis@users.noreply.github.com>
2022-12-03 23:19:40 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    //exercicio 1
+    /*System.out.println("Informe um valor");
+    Scanner scan = new Scanner(System.in);
+    int valorUm = scan.nextInt();
+    if(valorUm < 0){
+     System.out.println("O número informado é negativo"); 
+    }
+    else if(valorUm > 0){
+      System.out.println("O número informado é positivo");
+    }
+    else{
+      System.out.println("O valor informado é zero");
+    }*/
+
+    //exercico 2
+    /*System.out.println("informe um valor");
+    Scanner scan = new Scanner(System.in);
+    int valorUm = scan.nextInt();
+    
+    System.out.println("informe um segundo valor");
+    int valorDois = scan.nextInt();
+    
+    System.out.println("informe um terceiro valor");
+    int valorTres = scan.nextInt();
+
+    if (valorUm > valorDois && valorUm > valorTres){
+       System.out.println("O maior valor informado foi: " + valorUm);
+    }
+    else if(valorDois > valorUm && valorDois > valorTres){
+       System.out.println("O maior valor informado foi: " + valorDois);
+    }
+    else{
+      System.out.println("O maior valor informado foi: " + valorTres);
+    }*/
+    
+    //exercicio 3
+    /*System.out.println("informe um valor");
+    Scanner scan = new Scanner(System.in);
+    int valorUm = scan.nextInt();
+    
+    System.out.println("informe um segundo valor");
+    int valorDois = scan.nextInt();
+    
+    System.out.println("informe um terceiro valor");
+    int valorTres = scan.nextInt();
+    
+    if(valorUm < valorDois && valorUm < valorTres){
+      System.out.println("Soma = " + (valorDois + valorTres));
+    };
+    else if(valorDois < valorTres && valorDois < valorUm){
+      System.out.println("Soma = " + (valorTres + valorUm));
+    };
+    else{
+      System.out.println("Soma = " + (valorDois + valorUm));
+    };*/
+
+    //exercicio 4
+    /*System.out.println("informe um valor");
+    Scanner scan = new Scanner(System.in);
+    int valorUm = scan.nextInt();
+    
+    System.out.println("informe um segundo valor");
+    int valorDois = scan.nextInt();
+    
+    System.out.println("informe um terceiro valor");
+    int valorTres = scan.nextInt();
+
+    System.out.println("informe um quarto valor");
+    int valorQuatro = scan.nextInt();
+
+    System.out.println("informe um quinto valor");
+    int valorCinco = scan.nextInt();
+
+    System.out.println("informe um Sexto valor");
+    int valorSeis = scan.nextInt();
+
+    System.out.println("informe um sétimo valor");
+    int valorSete = scan.nextInt();
+
+    System.out.println("informe um oitavo valor");
+    int valorOito = scan.nextInt();
+
+    System.out.println("informe um nono valor");
+    int valorNove = scan.nextInt();
+
+    System.out.println("informe um decimo valor");
+    int valorDez = scan.nextInt();
+
+    System.out.println("A média dos valores informados é: " + (valorUm + valorDois + valorTres + valorQuatro + valorCinco + valorSeis + valorSete + valorOito + valorNove + valorDez)/2);
+  }*/
+
+    //exercicio 5
+    /*System.out.println("informe um valor");
+    Scanner scan = new Scanner(System.in);
+    int valorUm = scan.nextInt();
+    
+    System.out.println("informe um segundo valor");
+    int valorDois = scan.nextInt();
+    
+    System.out.println("informe um terceiro valor");
+    int valorTres = scan.nextInt();
+
+    System.out.println("informe um quarto valor");
+    int valorQuatro = scan.nextInt();
+
+    int soma = (valorUm + valorDois + valorTres + 
+     valorQuatro)/4;
+    
+    if(soma >= 6){
+      System.out.println("Parabéns, você foi aprovado");
+    }
+    else{
+      System.out.println("Desculpe mas você foi reprovado");
+    };*/
+  }
+}
